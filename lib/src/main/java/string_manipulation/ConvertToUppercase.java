1c1d9e959e244d4bf0b169b80d196c41647f608b
DELL <DELL@DESKTOP-501F2HP>
2022-10-13 14:52:02 +0600

    Added some source files

---
+++ lib/src/main/java/string_manipulation/ConvertToUppercase.java
@@ -0,0 +1,33 @@
+/* 
+ * 8. Write a program to convert each 1st char to uppercase from a string
+ * 
+ * Input: rahim lives in sylhet
+   Output: Rahim Lives in Sylhet
+ */
+
+package string_manipulation;
+
+import java.util.Scanner;
+
+public class ConvertToUppercase {
+
+	public static void main(String[] args) {
+		try {
+			Scanner user_input = new Scanner(System.in);
+			System.out.print("Please enter a string: ");
+			String str = user_input.nextLine();
+			String firstLetter = " ";
+			String othersLetter = " ";
+			String[] words = str.split(" ");
+			for (int i = 0; i < words.length; i++) {
+				firstLetter = String.valueOf(words[i].charAt(0)).toUpperCase();
+				othersLetter = words[i].substring(1);
+				System.out.println(firstLetter + othersLetter);
+			}
+		} catch (Exception e) {
+			System.out.println("Please enter a line " + e);
+		}
+
+	}
+
+}
---
+++ lib/src/main/java/string_manipulation/HowManyImages.java
@@ -0,0 +1,17 @@
+/*
+ * 7. Find out how many images are in the given array:
+ * Input: 
+ * ["photo1.jpg", "doc1.pdf", "doc2.docx", "photo2.png", "doc3.pdf", "msoffice.exe", "photo3.jpg"]
+   Output: 3
+ */
+
+package string_manipulation;
+
+public class HowManyImages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
