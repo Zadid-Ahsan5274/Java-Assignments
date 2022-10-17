bbab58b5319c9a9cc1e101388d00723d9fb3aa9e
Zadid-Ahsan5274 <zadid.ahsan5274@gmail.com>
2022-10-17 19:37:18 +0600

    Modified how many images file

--- lib/src/main/java/string_manipulation/HowManyImages.java
+++ lib/src/main/java/string_manipulation/HowManyImages.java
@@ -6,24 +6,33 @@
  */
 
 package string_manipulation;
-
 public class HowManyImages {
 
     public static void main(String[] args) {
 
-        String filesList[] = { "photo1.jpg", "doc1.pdf", "doc2.docx", "photo2.png", "doc3.pdf", "msoffice.exe", "photo3.jpg" };
+        String filesList[] = {"photo1.jpg", "doc1.pdf", "doc2.docx", "photo2.png", "doc3.pdf", "msoffice.exe", "photo3.jpg"};
         String extensionList[] = new String[filesList.length];
-        int count = 0;
+        int countOfImages = 0;
         for (int i = 0; i < filesList.length; i++) {
-            for (int j = 0; j < extensionList.length; j++) {
-                extensionList[j] = String.valueOf(filesList[i].split("."));
-                System.out.println(extensionList[j]);
-                if (extensionList[j] == ".jpg" || extensionList[j] == ".png") {
-                    count++;
-                }
+            if (i == 0 || i == 3 || i == 6) {
+                extensionList[i] = filesList[i].substring(6, 10).toString();
+            } else if (i == 1 || i == 2 || i == 4) {
+                extensionList[i] = filesList[i].substring(4, 8).toString();
+            } else if (i == 5) {
+                extensionList[i] = filesList[i].substring(8, 12).toString();
+            }
+            //System.out.println(extensionList[i]);
+
+        }
+        for(int i = 0; i < extensionList.length;i++){
+            if (extensionList[i].toString().equals(".jpg") || extensionList[i].toString().equals(".png")) {
+                countOfImages++;
             }
         }
-        System.out.println(count);
+
+        System.out.println("Number of images: "+countOfImages);
+
+
     }
 
 }
