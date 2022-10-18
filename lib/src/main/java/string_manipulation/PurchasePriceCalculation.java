b19f9a2ce4d8aee58bccf9a0c0b57740d880b4c1
Zadid-Ahsan5274 <zadid.ahsan5274@gmail.com>
2022-10-18 07:49:58 +0600

    Added purchase price calculation problem solution

--- lib/src/main/java/string_manipulation/PurchasePriceCalculation.java
+++ lib/src/main/java/string_manipulation/PurchasePriceCalculation.java
@@ -10,20 +10,17 @@ public class PurchasePriceCalculation {
     public static void main(String[] args) {
 
         String str = "Core i 7 HP laptop price is 76000 tk and cash payment discount 10%.";
-
-        str = str.replaceAll("[^\\d]", " ");
-        str = str.trim();
-        str = str.replace("7 ", " ");
-
-        str = str.trim();
-        System.out.println(str);
-        String arr[] = str.split(" ");
+        String digits = str.replaceAll("\\D+", " ");
+        System.out.println(digits);
+        digits = digits.replaceAll(" 7 "," ");
+        System.out.println(digits);
+        digits = digits.trim();
+        System.out.println(digits);
+        String[]arr = digits.split(" ");
         double laptop_price = Double.parseDouble(arr[0]);
-        System.out.println(laptop_price);
-        /*double discount = Double.parseDouble(arr[1]);
-        System.out.println(discount);*/
-       double purchase_price = laptop_price - 7600;
-        System.out.println(purchase_price);
+        double discount_amount = Double.parseDouble(arr[1]);
+        double purchase_price = laptop_price - (laptop_price * (discount_amount/100));
+        System.out.println("Purchase price is: "+purchase_price);
 
     }
 
